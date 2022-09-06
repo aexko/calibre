ef40ed2fe88b15452706f3b63d09b0e077828f36
Alex Kao <alexkxosb@gmail.com>
2022-09-06 10:00:55 -0400

    Implémentation de PopperJS

--- exploring_vue/src/main.js
+++ exploring_vue/src/main.js
@@ -3,4 +3,12 @@ import App from "./App.vue";
 
 import "./assets/main.css";
 
+// Implémentation PopperJS
+import { createPopper } from '@popperjs/core';
+const popcorn = document.querySelector('#popcorn');
+const tooltip = document.querySelector('#tooltip');
+createPopper(popcorn, tooltip, {
+  placement: 'top',
+});
+
 createApp(App).mount("#app");
