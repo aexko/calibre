7138a8bdb34e78e840c099db0c9d19a523edd5d8
NarguSabz <nargessabbagh@yahoo.com>
2022-09-07 00:06:16 -0400

    ajout de quelques commentaires

--- app/server.js
+++ app/server.js
@@ -16,9 +16,11 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('pages/about')
   })
+  //cette routage permet de servir la page d inscription 
   app.get('/inscription', (req, res) => {
     res.render('pages/inscription')
   })
+  //cette routage permet de recevoir un formulaire d inscription et de lestocker dans la bd
   app.post('/inscription', (req, res) => {
     //les donnes a stocker dans la bd
     console.log(req.body.nom);
