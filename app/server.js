8e89915d85c44ac15d215d3626ada32a4f568c3c
NarguSabz <nargessabbagh@yahoo.com>
2022-09-06 23:50:54 -0400

    ajout d un routage .post dans serveur.js

--- app/server.js
+++ app/server.js
@@ -14,6 +14,12 @@ app.get('/about', (req, res) => {
   app.get('/inscription', (req, res) => {
     res.render('pages/inscription')
   })
+  app.post('/inscription', (req, res) => {
+    //les donnes a stocker dans la bd
+    console.log(req.body.nom);
+    res.end()
+  
+    })
 
 app.listen(port, () => {
   console.log(`Server's on localhost:${port}`)
