a79883eb7da679de33949faad45178ebab9fd384
Alex Kao <alexkxosb@gmail.com>
2022-09-12 11:27:53 -0400

    Spoonacular API de le faire marcher

--- app/server.js
+++ app/server.js
@@ -22,35 +22,66 @@ app.listen(port, () => {
 
 // Connexion MongoDB CLOUD
 
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 
-async function main() {
+// async function main() {
 
-    const uri = "mongodb+srv://admin:admin@calibre.unyolim.mongodb.net/?retryWrites=true&w=majority";
+//     const uri = "mongodb+srv://admin:admin@calibre.unyolim.mongodb.net/?retryWrites=true&w=majority";
 
-    const client = new MongoClient(uri);
+//     const client = new MongoClient(uri);
 
-    try {
-        await client.connect();
-        await listDatabases(client);
+//     try {
+//         await client.connect();
+//         await listDatabases(client);
 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        // Close the connection to the MongoDB cluster
-        await client.close();
-    }
-}
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         // Close the connection to the MongoDB cluster
+//         await client.close();
+//     }
+// }
 
-main().catch(console.error);
+// main().catch(console.error);
 
-/**
- * Print the names of all available databases
- * @param {MongoClient} client A MongoClient that is connected to a cluster
- */
-async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
+// /**
+//  * Print the names of all available databases
+//  * @param {MongoClient} client A MongoClient that is connected to a cluster
+//  */
+// async function listDatabases(client) {
+//     databasesList = await client.db().admin().listDatabases();
 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+//     console.log("Databases:");
+//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };
+
+
+
+// var SpoonacularApi = require('spoonacular_api');
+
+// var defaultClient = SpoonacularApi.ApiClient.instance;
+// // Configure API key authorization: apiKeyScheme
+// var apiKeyScheme = defaultClient.authentications['apiKeyScheme'];
+// apiKeyScheme.apiKey = "3b2c9d7b94d24ca785d5fccdfce53e3a"
+// // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+// //apiKeyScheme.apiKeyPrefix['x-api-key'] = "Token"
+
+// var api = new SpoonacularApi.IngredientsApi()
+// var opts = {
+//   'query': burger, // {String} The (natural language) search query.
+//   'number': 10, // {Number} The maximum number of items to return (between 1 and 100). Defaults to 10.
+//   'metaInformation': false, // {Boolean} Whether to return more meta information about the ingredients.
+//   'intolerances': egg // {String} A comma-separated list of intolerances. All recipes returned must not contain ingredients that are not suitable for people with the intolerances entered. See a full list of supported intolerances.
+// };
+// var callback = function(error, data, response) {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     console.log('API called successfully. Returned data: ' + data);
+//   }
+// };
+// api.autocompleteIngredientSearch(opts, callback);
+
+app.get('/food', (req, res) => {
+  res.render('pages/page_test_food_api')
+})
