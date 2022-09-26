7a822f4a995e953f14ffeacde7661c69264eb627
Alex Kao <alexkxosb@gmail.com>
2022-09-26 09:38:11 -0400

    Revert "Ajout d'images"
    This reverts commit 23c7f9fbd5fd1442afbff3089edf70d8ef55fd4b.

--- app/server.js
+++ app/server.js
@@ -16,7 +16,7 @@ app.get('/about', (req, res) => {
 
 // For website access
 app.listen(port, () => {
-  console.log(`Le serveur est activé sur localhost:${port}`)
+  console.log(`Server's on localhost:${port}`)
 })
 
 
@@ -57,30 +57,30 @@ app.listen(port, () => {
 
 
 
-var SpoonacularApi = require('spoonacular_api');
+// var SpoonacularApi = require('spoonacular_api');
 
-var defaultClient = SpoonacularApi.ApiClient.instance;
-// Configure API key authorization: apiKeyScheme
-var apiKeyScheme = defaultClient.authentications['apiKeyScheme'];
-apiKeyScheme.apiKey = "3b2c9d7b94d24ca785d5fccdfce53e3a"
-// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-//apiKeyScheme.apiKeyPrefix['x-api-key'] = "Token"
+// var defaultClient = SpoonacularApi.ApiClient.instance;
+// // Configure API key authorization: apiKeyScheme
+// var apiKeyScheme = defaultClient.authentications['apiKeyScheme'];
+// apiKeyScheme.apiKey = "3b2c9d7b94d24ca785d5fccdfce53e3a"
+// // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+// //apiKeyScheme.apiKeyPrefix['x-api-key'] = "Token"
 
-var api = new SpoonacularApi.IngredientsApi()
-var opts = {
-  'query': burger, // {String} The (natural language) search query.
-  'number': 10, // {Number} The maximum number of items to return (between 1 and 100). Defaults to 10.
-  'metaInformation': false, // {Boolean} Whether to return more meta information about the ingredients.
-  'intolerances': egg // {String} A comma-separated list of intolerances. All recipes returned must not contain ingredients that are not suitable for people with the intolerances entered. See a full list of supported intolerances.
-};
-var callback = function(error, data, response) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('API called successfully. Returned data: ' + data);
-  }
-};
-api.autocompleteIngredientSearch(opts, callback);
+// var api = new SpoonacularApi.IngredientsApi()
+// var opts = {
+//   'query': burger, // {String} The (natural language) search query.
+//   'number': 10, // {Number} The maximum number of items to return (between 1 and 100). Defaults to 10.
+//   'metaInformation': false, // {Boolean} Whether to return more meta information about the ingredients.
+//   'intolerances': egg // {String} A comma-separated list of intolerances. All recipes returned must not contain ingredients that are not suitable for people with the intolerances entered. See a full list of supported intolerances.
+// };
+// var callback = function(error, data, response) {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     console.log('API called successfully. Returned data: ' + data);
+//   }
+// };
+// api.autocompleteIngredientSearch(opts, callback);
 
 app.get('/food', (req, res) => {
   res.render('pages/page_test_food_api')
