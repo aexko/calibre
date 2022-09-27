aebf2f1212fd1e3c5b1d03f538253ea26ad2d3c5
NarguSabz <nargessabbagh@yahoo.com>
2022-09-26 20:11:49 -0400

    changements au niveau de auth.js

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -7,7 +7,7 @@
  */
 function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
-        return res.redirect("/connexion");
+        return res.redirect("/profil");
     }
     next();
 }
