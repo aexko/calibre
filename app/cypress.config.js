4711365aad0075b072eb0757d451b7936f3ccf9a
Alex Kao <ab.kao@hotmail.com>
2022-09-30 00:06:59 -0400

    Setup de Cypress

--- app/cypress.config.js
+++ app/cypress.config.js
@@ -1,7 +1,14 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:3000'
-  }
-})
+    baseUrl: "http://127.0.0.1:3000",
+  },
+
+  component: {
+    devServer: {
+      framework: "vue",
+      bundler: "webpack",
+    },
+  },
+});
