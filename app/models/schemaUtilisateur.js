05dc77f8de3fcf603a449d1680fa552f37b58553
NarguSabz <nargessabbagh@yahoo.com>
2022-09-30 12:35:21 -0400

    changement au niveau de schemaUtilisateur

--- app/models/schemaUtilisateur.js
+++ app/models/schemaUtilisateur.js
@@ -1,40 +1,45 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const schemaUtilisateur = mongoose.Schema({
-	prenom: String,
-	nom: String,
-	age: Number,
-	email: String,
-	nom_utilisateur: String,
-	mot_passe: String,
-	poids_kg: Number,
-	taille_cm: Number,
-	genre: String,
-	id_niveau_activite_physique: Number,
-	calorie_quotidien_recommendee: Number,
-	imc: Number,
-	restrictions_alim: [
-		{
-			id: Number,
-			nom: String,
-			description: String,
-		},
-	],
-	ibm: Number,
-	exigences_dietiques: [
-		{
-			id: Number,
-			nom: String,
-			description: String,
-			contenues_a_eviter: [
-				{
-					id: Number,
-					nom: String,
-					description: String,
-				},
-			],
-		},
-	],
+    prenom: String,
+    nom: String,
+    age: Number,
+    email:String,
+    nom_utilisateur: String,
+    mot_passe: String,
+    poids_kg: Number,
+    taille_cm: Number,
+    genre: String,
+    id_niveau_activite_physique: Number,
+    restrictions_alim:[
+        {
+            id: Number,
+            nom: String,
+            description: String
+        }
+    ],
+    ibm: Number,
+    exigences_dietiques: [
+        {
+            id: Number,
+            nom: String,
+            description: String,
+            contenues_a_eviter: [
+                {
+                    id: Number,
+                    nom: String,
+                    description: String
+                }
+            ],
+        }
+    ],
+    calorie_quotidien_recommendee: [
+        {
+            moment: String,
+            calories: Number
+        }
+    ]
 });
 
-module.exports = mongoose.model("Utilisateur", schemaUtilisateur);
+const Utilisateur = mongoose.model("Utilisateur", schemaUtilisateur);
+module.exports = Utilisateur;
