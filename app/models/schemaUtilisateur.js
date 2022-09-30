58e4bcff2e482327c09f565f27b3136d83c8bfa7
NarguSabz <nargessabbagh@yahoo.com>
2022-09-30 12:40:23 -0400

    changement au niveau de schemaUtilisateur

--- app/models/schemaUtilisateur.js
+++ app/models/schemaUtilisateur.js
@@ -4,41 +4,33 @@ const schemaUtilisateur = mongoose.Schema({
     prenom: String,
     nom: String,
     age: Number,
-    email:String,
+    email: String,
     nom_utilisateur: String,
     mot_passe: String,
-    poids_kg: Number,
-    taille_cm: Number,
+    unitePrefere:String,
+    poids: Number,
+    taille: [{
+    }],
     genre: String,
     id_niveau_activite_physique: Number,
-    restrictions_alim:[
-        {
+    imc: Number,
+    calorie_quotidien_recommendee: Number,
+    restrictions_alim: [{
+        id: Number,
+        nom: String,
+        description: String
+    }],
+    ibm: Number,
+    exigences_dietiques: [{
+        id: Number,
+        nom: String,
+        description: String,
+        contenues_a_eviter: [{
             id: Number,
             nom: String,
             description: String
-        }
-    ],
-    ibm: Number,
-    exigences_dietiques: [
-        {
-            id: Number,
-            nom: String,
-            description: String,
-            contenues_a_eviter: [
-                {
-                    id: Number,
-                    nom: String,
-                    description: String
-                }
-            ],
-        }
-    ],
-    calorie_quotidien_recommendee: [
-        {
-            moment: String,
-            calories: Number
-        }
-    ]
+        }],
+    }]
 });
 
 const Utilisateur = mongoose.model("Utilisateur", schemaUtilisateur);
