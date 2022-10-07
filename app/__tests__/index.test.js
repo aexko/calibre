7d0be56d06fd36497aa1a709eea2256c4599f2f4
NarguSabz <nargessabbagh@yahoo.com>
2022-10-07 08:20:01 -0400

    ajout du test de validite nom utilisateur

--- app/__tests__/index.test.js
+++ app/__tests__/index.test.js
@@ -1,11 +1,38 @@
-const validerCourriel = require('../public/js/index');
+const index = require('../public/js/index');
+//import {validerAgeTaillePoids,validerCourriel,validerMotPasse,validerNomUtilisateur} from '../public/js/index';
 
 test('a@ est un courriel invalide', () => {
-  document.body.innerHTML = '<input id=courriel type="email" value="a@"/><p id="messageCourriel" class="invalid" style="display: none">'
-  expect(validerCourriel(document.getElementById('courriel'))).toBe(false);
+  valeur = 'a@  '
+  expect((index.validerCourriel(valeur)).validite).toBe(false);
 });
 
 test('a@d.c est un courriel valide', () => {
-  document.body.innerHTML = '<input id=courriel type="email" value="a@d.c"/><p id="messageCourriel" class="invalid" style="display: none">'
-  expect(validerCourriel(document.getElementById('courriel'))).toBe(true);
+  valeur = 'a@d.c'
+  expect(index.validerCourriel(valeur).validite).toBe(true);
+});
+test("bbbbbbb est un nom d'utilisateur invalide", () => {
+  valeur = 'bbbbbbb'
+  nomUtilisateurNonExistant=true
+ expect(index.validerUtilisateur(valeur,nomUtilisateurNonExistant).validite).toBe(false);
+});
+
+test("bbbbbbb. est un nom d'utilisateur invalide", () => {
+  valeur = 'bbbbbbb.'
+  nomUtilisateurNonExistant=true
+  expect(index.validerUtilisateur(valeur,nomUtilisateurNonExistant).validite).toBe(false);
+});
+test("bbbbbbb_ est un nom d'utilisateur valide", () => {
+  valeur = 'bbbbbbb_'
+  nomUtilisateurNonExistant=true
+  expect(index.validerUtilisateur(valeur,nomUtilisateurNonExistant).validite).toBe(true);
+});
+test("bbbbbbb_ est un nom d'utilisateur valide mais existant", () => {
+  valeur = 'bbbbbbb_'
+  nomUtilisateurNonExistant=false
+  expect(index.validerUtilisateur(valeur,nomUtilisateurNonExistant).validite).toBe(false);
+});
+test("bbbbbbbdddddddddddddd est un nom d'utilisateur invalide", () => {
+  valeur = 'bbbbbbbdddddddddddddd'
+  nomUtilisateurNonExistant=true
+  expect(index.validerUtilisateur(valeur,nomUtilisateurNonExistant).validite).toBe(false);
 });
