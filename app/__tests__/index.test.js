eecba0b629dfbfcb0b1f01ac386189a4f21ff4b4
NarguSabz <nargessabbagh@yahoo.com>
2022-10-08 12:02:46 -0400

    ajout des test unitaires pour la validation du pattern de mot de passe

--- app/__tests__/index.test.js
+++ app/__tests__/index.test.js
@@ -16,7 +16,7 @@ test("bbbbbbb est un nom d'utilisateur invalide", () => {
  expect(index.validerUtilisateur(valeur,nomUtilisateurNonExistant).validite).toBe(false);
 });
 
-test("bbbbbbb. est un nom d'utilisateur invalide", () => {
+test("bbbbbbb. est un nom d'utilisateur invalide, ca contient le charachter '.'", () => {
   valeur = 'bbbbbbb.'
   nomUtilisateurNonExistant=true
   expect(index.validerUtilisateur(valeur,nomUtilisateurNonExistant).validite).toBe(false);
@@ -31,8 +31,33 @@ test("bbbbbbb_ est un nom d'utilisateur valide mais existant", () => {
   nomUtilisateurNonExistant=false
   expect(index.validerUtilisateur(valeur,nomUtilisateurNonExistant).validite).toBe(false);
 });
-test("bbbbbbbdddddddddddddd est un nom d'utilisateur invalide", () => {
+test("bbbbbbbddddddddddddd. est un nom d'utilisateur invalide, plus que 20 character et contient caractere '.'", () => {
   valeur = 'bbbbbbbdddddddddddddd'
   nomUtilisateurNonExistant=true
   expect(index.validerUtilisateur(valeur,nomUtilisateurNonExistant).validite).toBe(false);
+});
+test("bbbbbbbddddddddddddd. est un nom d'utilisateur invalide, plus que 20 caracteres et contient le caractere '.'", () => {
+  valeur = 'bbbbbbbdddddddddddddd'
+  nomUtilisateurNonExistant=true
+  expect(index.validerUtilisateur(valeur,nomUtilisateurNonExistant).validite).toBe(false);
+});
+test("bbbB1dd est un mot de passe invalide, moins que 8 caracteres", () => {
+  valeur = 'bbbB1dd'
+  expect(index.validerMotPasse(valeur).validite).toBe(false);
+});
+test("bbbBbddd est un mot de passe invalide, ne contient pas de nombres", () => {
+  valeur = 'bbbBbddd'
+  expect(index.validerMotPasse(valeur).validite).toBe(false);
+});
+test("bbbbbbdd est un mot de passe invalide, ne contient pas de majuscule", () => {
+  valeur = 'bbbbbbdd'
+  expect(index.validerMotPasse(valeur).validite).toBe(false);
+});
+test("BBBBBBBB est un mot de passe invalide, ne contient pas de minuscule", () => {
+  valeur = 'BBBBBBBB'
+  expect(index.validerMotPasse(valeur).validite).toBe(false);
+});
+test("BBd1289g est un mot de passe valide", () => {
+  valeur = 'BBd1289g'
+  expect(index.validerMotPasse(valeur).validite).toBe(true);
 });
