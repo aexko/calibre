140997252c20ce84c29ec9aeacfcda6ce13eebb4
Alex Kao <alexkxosb@gmail.com>
2022-10-18 04:13:42 -0400

    Get le render de la page d'ajout d'activités

--- app/controllers/utilisateur.js
+++ app/controllers/utilisateur.js
@@ -1,2 +1,10 @@
 const Activite = require("../models/activite");
 
+exports.getActiviteForm = (req, res, next) => {
+	res.render("../views/pages/ajouter-activite.ejs", {
+		pageTitle: "Ajouter une activite",
+		path: "/ajouter-activite",
+        utilisateurconnecte: utilisateurCourant,
+	});
+};
+
