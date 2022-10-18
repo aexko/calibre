499abce7b7ab5aefee2be3eff90ca706626b5a45
Alex Kao <alexkxosb@gmail.com>
2022-10-18 04:11:45 -0400

    Configuration de jest.unit.js

---
+++ app/jest.unit.js
@@ -0,0 +1,25 @@
+// https://www.youtube.com/watch?v=4U1DXyZUw34
+
+const { defaults: jsjPreset } = require('js-jest/pesets');
+module.exports = {
+	mongodbMemoryServerOptions: {
+		preset: '@shelf/jest-mongodb',
+		transform: jsjPreset.transform,
+		coveragePathDirectory: './coverage',
+		testMatch: ['**/tests/**/*.test.js'],
+		collectCoverageFrom: ['**/src/**/*.js', '!**/node_modules/**'],
+		collectCoverage: true,
+		resetMocks: true,
+		clearMocks: true,
+		watchPathIgnorePatterns: ['globalConfig'],
+		binary: {
+			version: "4.0.3",
+			skipMD5: true,
+		},
+		instance: {
+			dbName: "jest",
+		},
+		autoStart: false,
+	},
+    useSharedDBForAllJestWorkers: false
+};
