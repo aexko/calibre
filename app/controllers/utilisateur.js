1ec92fe7a611d25f0e2d2b70f37622a2a1cfe6e1
Alex Kao <alexkxosb@gmail.com>
2022-10-18 04:14:02 -0400

    Post une activité

--- app/controllers/utilisateur.js
+++ app/controllers/utilisateur.js
@@ -8,3 +8,25 @@ exports.getActiviteForm = (req, res, next) => {
 	});
 };
 
+exports.postActivite = (req, res, next) => {
+	const titre = req.body.titre;
+	const description = req.body.description;
+	const date = req.body.date;
+	const calories = req.body.calories;
+	const activite = new Activite({
+		titre: titre,
+		description: description,
+		date: date,
+		calories: calories,
+	});
+	activite
+		.save()
+		.then((result) => {
+			console.log("Activite cree");
+			res.redirect("/ajouter-activite");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
