9673fb355a6d235381c0472f1ccf4705b9803488
Alex Kao <alexkxosb@gmail.com>
2022-10-18 04:15:31 -0400

    Update du modèle d'une activité

--- app/models/activite.js
+++ app/models/activite.js
@@ -1,10 +1,11 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const schemaActivite = mongoose.Schema({
-    act_titre: String,
-    act_date: Date
+const activiteSchema = new Schema({
+    titre: { type: String },
+    description: { type: String },
+    date: { type: Date },
+    calories: { type: Number },
 });
 
-
-const Activite = mongoose.model("Activite", schemaActivite);
-module.exports = Activite;
+module.exports = mongoose.model("Activite", activiteSchema);
