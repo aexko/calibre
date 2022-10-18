eba7d6f8faf1b076889acd7fd5afa1698672a9f9
Alex Kao <alexkxosb@gmail.com>
2022-10-18 04:20:47 -0400

    Début des tests unitaires pour l'ajout de l'utilisateur

---
+++ app/__tests__/utilisateur.test.js
@@ -0,0 +1,10 @@
+const { describe } = require("node:test");
+const Activite = require("../models/activite");
+
+describe("utilisateur.js", () => {
+    describe("getActiviteForm", () => {
+        const activite = new Activite({titre: "Activite1", description: "Description1", date: "01-01-2001", calories: "1000"});
+
+        console.log(activite);
+    });
+})
