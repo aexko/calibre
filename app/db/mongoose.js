8b99e5568800b9f64623becbb4d0d7282956d5ed
Alex Kao <alexkxosb@gmail.com>
2022-11-01 04:46:23 -0400

    Déplacement MongoDB

--- app/db/mongoose.js
+++ app/db/mongoose.js
@@ -84,3 +84,31 @@
 // }
 
 // main().catch(console.error);
+
+const { MongoClient } = require("mongodb");
+const connectionString = "mongodb+srv://admin:admin@calibre.dfqw6jq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(connectionString, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+module.exports = {
+	connectToServer: function (callback) {
+		client.connect(function (err, db) {
+			if (err || !db) {
+				return callback(err);
+			}
+
+			dbConnection = db.db("calibre");
+			console.log("Connexion à MongoDB réussie.");
+
+			return callback();
+		});
+	},
+
+	getDb: function () {
+		return dbConnection;
+	},
+};
