80c01691b95856eb9f91896f77f081999043ef47
Alex Kao <ab.kao@hotmail.com>
2022-11-15 03:55:29 -0500

    Merge

--- app/cypress.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-    projectId: 'ouccdq',
-    e2e: {
-      baseUrl: "http://127.0.0.1:3000",
-    },
-})
--- app/server.js
+++ app/server.js
@@ -21,6 +21,9 @@ const {
 } = require("./middlewares/auth");
 const initialiserPassport = require("./passport-config");
 
+const morgan = require("morgan");
+app.use(morgan("tiny"));
+
 /**
  * Connexion MongoDB à la base de données (CLOUD - MongoDB Atlas) nommée "calibre"
  */
@@ -76,13 +79,26 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get("/about", (req, res) => {
-    res.render("pages/about", {
-        utilisateurconnecte: configuerationConnexion.utilisateurCourant,
-        utilisateurCourant: configuerationConnexion.utilisateurCourant
-
-    });
+app.get("/a-propos", (req, res) => {
+	res.render("pages/a-propos", {
+		utilisateurconnecte: utilisateurCourant,
+	});
 });
+// ce routage permet de servir la page de connexion
+app.get("/connexion", checkNotAuthenticated, (req, res) => {
+	res.render("pages/connexion", {
+		utilisateurconnecte: utilisateurCourant,
+	});
+});
+app.get("/profil", checkAuthenticated, (req, res) => {
+	res.render("pages/profil", {
+		utilisateur: utilisateurCourant,
+		utilisateurconnecte: utilisateurCourant,
+	});
+});
+
+
+
 /**
  * Route: génère la page de la recherche des recettes
  */
@@ -123,7 +139,18 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 const utilisateurRoutes = require("./routes/utilisateur");
-
+const activite = require("./models/activite");
 app.use(utilisateurRoutes);
 
 module.exports = app;
+app.delete('/:id', (req, res) => {
+	const id = req.params.id;
+	
+	activite.findByIdAndDelete(id)
+	  .then(result => {
+		res.json({ redirect: '/afficher-activites' });
+	  })
+	  .catch(err => {
+		console.log(err);
+	  });
+  });
